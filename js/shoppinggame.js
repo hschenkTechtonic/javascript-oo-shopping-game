f677431a23b152da8c3ed10d70e8f10df9105e40
hschenkTechtonic <hannah.schenk@techtonic.com>
2020-10-29 15:52:46 -0500

    finished

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -211,10 +211,17 @@ function loadMasterData() {
 }
 
 // Complete this function
-const findProductById = (id) => {};
+const findProductById = (id) => {
+    return function(product){
+        return product.id == id;
+    }
+};
 
 // Complete this function
-const generateProductId = () => {};
+const generateProductId = () => {
+    return Math.floor(Math.random() * (Math.floor(20) 
+    - Math.ceil(1) + 1) + 1;
+};
 
 
 const getProduct = (prodList, pId) => {
